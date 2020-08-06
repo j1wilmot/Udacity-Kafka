6e601c39d4b960ff43d4c01f181c466f33e8ab57
Jeremy Wilmot <jeremy.wilmot@infogroup.com>
2020-08-06 12:37:12 -0700

    Step 1: Add producer code

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -9,6 +9,9 @@ from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
 
+BROKER_URLS = "PLAINTEXT://localhost:9092" # Only one host specified in docker compose
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
+TOPIC_NAMESPACE = "org.chicago.cta"
 
 class Producer:
     """Defines and provides common functionality amongst Producers"""
@@ -25,54 +28,29 @@ class Producer:
         num_replicas=1,
     ):
         """Initializes a Producer object with basic settings"""
-        self.topic_name = topic_name
+        self.topic_name = f"{TOPIC_NAMESPACE}.{topic_name}"
         self.key_schema = key_schema
         self.value_schema = value_schema
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
-        self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
-        }
+        self.admin_client = AdminClient({'bootstrap.servers': BROKER_URLS})
 
         # If the topic does not already exist, try to create it
         if self.topic_name not in Producer.existing_topics:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer = AvroProducer({'bootstrap.servers': BROKER_URLS, 'schema.registry.url': SCHEMA_REGISTRY_URL})
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
-
-    def time_millis(self):
-        return int(round(time.time() * 1000))
+        new_topic = NewTopic(self.topic_name, num_partitions=self.num_partitions, replication_factor=self.num_replicas)
+        self.admin_client.create_topics([new_topic])
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        self.producer.flush()
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
